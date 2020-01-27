f83b7a5535fe56f50c88bbdef4f2c638492919cf
Grant Harris <grant@grantharris.ca>
2020-01-26 18:19:31 -0800

    Point to aurorajs

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as aurorajs from 'aurorajs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() {
+
+		let ad = new aurorajs.Device('10.0.0.200', 2872);
+
+		ad.connectedState()
+		.filter(state=>state)
+		.subscribe((s)=>{
+		    ad.sendCommand('REQUEST_CONTROL', {password: 'aurora'});
+		    ad.sendCommand('GET_SYSTEM_INFO');
+		});
+
+		ad.getResponsesForCommand('REQUEST_CONTROL').subscribe(()=>{
+		    console.log('Request Control');
+		});
+
+		ad.getResponsesForCommand('GET_AVAILABLE_PATTERNS').subscribe((command)=>{
+		    console.log('Patterns: ', command);
+		});
+
+		ad.getErrors().subscribe((command)=>{
+		    console.log('Errors!: ', command);
+		});
+
+		ad.controlState().subscribe((state)=>{
+		    console.log('Control state: ', state);
+		});
+
+		ad.connectedState().subscribe((state)=>{
+		    console.log('Connection state: ', state);
+		});
+
+		ad.getCommandResponses().subscribe((raw)=>{
+		    console.log('Command Responses ', raw);
+		});
+
+		ad.getNotifications().subscribe((raw)=>{
+		    console.log('Notifications ', raw);
+		});
+  }
 
 }
